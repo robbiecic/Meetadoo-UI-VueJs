8d44ce22efd4f94574d5cb5b2c34ec662d7cf195
Robert Cicero <robert.cicero.rc@gmail.com>
2020-02-04 09:41:18 +0000

    Everytime app loads or is refreshed, now make a call to check if authenticated so it set the isLoggedIn state object

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vuex from "vuex";
 import createPersistedState from "vuex-persistedstate";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
+import axios from "axios";
 
 Vue.config.productionTip = false;
 Vue.use(vuetify);
@@ -16,6 +17,9 @@ const store = new Vuex.Store({
   mutations: {
     setUser(state) {
       state.isLoggedOn = true;
+    },
+    unSetUser(state) {
+      state.isLoggedOn = false;
     }
   },
   plugins: [createPersistedState()]
@@ -25,5 +29,20 @@ new Vue({
   router,
   vuetify,
   store,
+  beforeMount() {
+    //When vue app is loaded or refreshed, we want to check if there is active HTTP cookie
+    //Can only do this by making an API call to the see if a cookie exists with a valid JWT token
+    console.log("Mounting app");
+    axios
+      .get("http://localhost:8080/Development/?action=isAuthenticated", {
+        withCredentials: true
+      })
+      .then(() => {
+        this.$store.commit("setUser");
+      })
+      .catch(() => {
+        this.$store.commit("unSetUser");
+      });
+  },
   render: h => h(App)
 }).$mount("#app");
