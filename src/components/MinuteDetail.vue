34540fe3042d98e2580ee73e1582c0a80b03b2a6
Robert Cicero <robert.cicero@iress.com>
2020-02-12 17:43:48 +0000

    Working on meeting detail form to be used to amend and add new meetings

--- src/components/MinuteDetail.vue
+++ src/components/MinuteDetail.vue
@@ -16,6 +16,7 @@
         ></v-text-field>
       </v-row>
       <v-row>
+        <!-- Start the date of meeting -->
         <v-col>
           <v-menu
             v-model="menu2"
@@ -30,6 +31,9 @@
                 v-model="minuteDetail.creation_date"
                 label="Event Date"
                 prepend-icon="mdi-event"
+                :shaped="shaped"
+                :outlined="outlined"
+                :rounded="rounded"
                 readonly
                 v-on="on"
               ></v-text-field>
@@ -37,11 +41,69 @@
             <v-date-picker v-model="minuteDetail.creation_date" @input="menu2 = false"></v-date-picker>
           </v-menu>
         </v-col>
+        <!-- Start the time_start of meeting -->
         <v-col>
-          <v-time-picker v-model="minuteDetail.time_start" type="month" width="290" class="ml-4"></v-time-picker>
+          <v-menu
+            ref="menu"
+            v-model="menu3"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            :return-value.sync="time"
+            transition="scale-transition"
+            offset-y
+            max-width="290px"
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                v-model="minuteDetail.time_start"
+                label="Start Time"
+                readonly
+                v-on="on"
+                :shaped="shaped"
+                :outlined="outlined"
+                :rounded="rounded"
+              ></v-text-field>
+            </template>
+            <v-time-picker
+              v-if="menu3"
+              v-model="minuteDetail.time_start"
+              full-width
+              @click:minute="$refs.menu.save(time)"
+            ></v-time-picker>
+          </v-menu>
         </v-col>
+        <!-- Start the time_end of meeting -->
         <v-col>
-          <v-time-picker v-model="minuteDetail.time_end" type="month" width="290" class="ml-4"></v-time-picker>
+          <v-menu
+            ref="menu"
+            v-model="menu4"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            :return-value.sync="time"
+            transition="scale-transition"
+            offset-y
+            max-width="290px"
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                v-model="minuteDetail.time_end"
+                label="Start Time"
+                readonly
+                v-on="on"
+                :shaped="shaped"
+                :outlined="outlined"
+                :rounded="rounded"
+              ></v-text-field>
+            </template>
+            <v-time-picker
+              v-if="menu4"
+              v-model="minuteDetail.time_end"
+              full-width
+              @click:minute="$refs.menu.save(time)"
+            ></v-time-picker>
+          </v-menu>
         </v-col>
       </v-row>
       <v-row>
@@ -75,6 +137,8 @@ export default {
       menu: false,
       modal: false,
       menu2: false,
+      menu3: false,
+      menu4: false,
       componentVisible: false,
       shaped: false,
       outlined: true,
