535d11b5f92b2aa6d58c73a496ea0b822132a6b4
Robert Cicero <robert.cicero@iress.com>
2020-02-12 08:19:32 +0000

    Getting closer, up to resolving names from email address

--- src/components/reusable/peoplePicker.vue
+++ src/components/reusable/peoplePicker.vue
@@ -1,11 +1,5 @@
 <template>
-  <v-combobox
-    v-model="guests"
-    :items="email_list"
-    label="Add someone"
-    multiple
-    chips
-  >
+  <v-combobox v-model="guests" :items="email_list" label="Add someone" multiple chips>
     <template v-slot:selection="data">
       <v-chip
         :key="JSON.stringify(data.item)"
@@ -14,11 +8,7 @@
         :disabled="data.disabled"
         @click:close="data.parent.selectItem(data.item)"
       >
-        <v-avatar
-          class="accent white--text"
-          left
-          v-text="data.item.slice(0, 1).toUpperCase()"
-        ></v-avatar>
+        <v-avatar class="accent white--text" left v-text="getAvatar(data)"></v-avatar>
         {{ item }}
       </v-chip>
     </template>
@@ -41,15 +31,33 @@ export default {
       disabled: false
     };
   },
+  computed: {
+    // people: function() {
+    //   axios
+    //     .get("http://localhost:8080/Development/?action=getUserList")
+    //     .then(response => {
+    //       //Avatar will show the name , hidden field for email address as the key
+    //       //Onclick of the user name we can derive the email address
+    //       const json_data = JSON.parse(response.data.replace(/'/g, '"'));
+    //       this.email_list = json_data.email_only;
+    //       this.name_list = json_data.name_only;
+    //       this.people = json_data.linked_list;
+    //       this.isUpdating = false;
+    //     })
+    //     .catch(error => {
+    //       console.log(error);
+    //       this.isUpdating = false;
+    //     });
+    // }
+  },
   watch: {
     isUpdating(val) {
       if (val) {
         setTimeout(() => (this.isUpdating = false), 3000);
       }
     },
-    guests: function(newVal, oldVal) {
-      console.log(newVal, oldVal);
-      //If this changes I need to pull through the names corresponding to the email addresses...
+    guests: function() {
+      //console.log(newVal, oldVal);
     }
   },
   methods: {
@@ -57,6 +65,20 @@ export default {
       const index = this.guests.indexOf(item.name);
       console.log("index", index);
       if (index >= 0) this.guests.splice(index, 1);
+    },
+    getAvatar(data) {
+      //Here we need to turn the email address in data.item to a name
+      console.log("data.item", data.item);
+      //console.log("this.people", this.people);
+      //Need to loop through people to find the name for the email address
+      var i = 0;
+      var index = this.people.findIndex(p => p.email == data.item);
+      console.log("index", index);
+      for (i = 0; i < this.people.length; i++) {
+        //console.log(this.people[i]);
+        //Need to look at email key and return the name.
+      }
+      return data.item.slice(0, 1).toUpperCase();
     }
   },
   created: function() {
@@ -69,7 +91,7 @@ export default {
         const json_data = JSON.parse(response.data.replace(/'/g, '"'));
         this.email_list = json_data.email_only;
         this.name_list = json_data.name_only;
-        this.people = json_data.linked_list;
+        this.people = json_data.link_list;
         this.isUpdating = false;
       })
       .catch(error => {
