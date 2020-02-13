1f4508dd1f4c0591f3f5876e5659a00f8f1bc7ef
Robert Cicero <robert.cicero@iress.com>
2020-02-13 07:59:16 +0000

    Working on adding a new meeting

--- src/components/MinuteDetail.vue
+++ src/components/MinuteDetail.vue
@@ -1,127 +1,139 @@
 <template>
   <v-form>
-    <v-badge bordered color="error" icon="mdi-lock" overlap>
-      <v-container>
-        <v-row>
-          <v-text-field
-            v-model="minuteDetail.title"
-            :label="'Meeting Title'"
-            :shaped="shaped"
-            :outlined="outlined"
-            :rounded="rounded"
-            :single-line="singleLine"
-            :filled="filled"
-            :flat="flat"
-            :counter="counterEn ? counter : false"
-            :dense="dense"
-          ></v-text-field>
-        </v-row>
-        <v-row>
-          <!-- Start the date of meeting -->
-          <v-col>
-            <v-menu
-              v-model="menu2"
-              :close-on-content-click="false"
-              :nudge-right="40"
-              transition="scale-transition"
-              offset-y
-              min-width="290px"
-            >
-              <template v-slot:activator="{ on }">
-                <v-text-field
-                  v-model="minuteDetail.creation_date"
-                  label="Event Date"
-                  prepend-icon="mdi-event"
-                  :shaped="shaped"
-                  :outlined="outlined"
-                  :rounded="rounded"
-                  readonly
-                  v-on="on"
-                ></v-text-field>
-              </template>
-              <v-date-picker v-model="minuteDetail.creation_date" @input="menu2 = false"></v-date-picker>
-            </v-menu>
-          </v-col>
-          <!-- Start the time_start of meeting -->
-          <v-col>
-            <v-menu
-              ref="menu"
-              v-model="menu3"
-              :close-on-content-click="false"
-              :nudge-right="40"
-              :return-value.sync="time"
-              transition="scale-transition"
-              offset-y
-              max-width="290px"
-              min-width="290px"
-            >
-              <template v-slot:activator="{ on }">
-                <v-text-field
-                  v-model="minuteDetail.time_start"
-                  label="Start Time"
-                  readonly
-                  v-on="on"
-                  :shaped="shaped"
-                  :outlined="outlined"
-                  :rounded="rounded"
-                ></v-text-field>
-              </template>
-              <v-time-picker
-                v-if="menu3"
+    <v-container>
+      <!-- Row for header and button -->
+      <v-row>
+        <v-col>
+          <h1>Meeting detail</h1>
+        </v-col>
+        <v-col class="text-right">
+          <v-btn class="ma-2" tile outlined color="warning" @click="clear">
+            <v-icon left>mdi-pencil</v-icon>Clear
+          </v-btn>
+          <v-btn class="ma-2" tile outlined color="green" @click="add">
+            <v-icon left>mdi-pencil</v-icon>Add
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-text-field
+          v-model="minuteDetail.title"
+          :label="'Meeting Title'"
+          :shaped="shaped"
+          :outlined="outlined"
+          :rounded="rounded"
+          :single-line="singleLine"
+          :filled="filled"
+          :flat="flat"
+          :counter="counterEn ? counter : false"
+          :dense="dense"
+        ></v-text-field>
+      </v-row>
+      <v-row>
+        <!-- Start the date of meeting -->
+        <v-col>
+          <v-menu
+            v-model="menu2"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                v-model="minuteDetail.creation_date"
+                label="Event Date"
+                prepend-icon="mdi-event"
+                :shaped="shaped"
+                :outlined="outlined"
+                :rounded="rounded"
+                readonly
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker v-model="minuteDetail.creation_date" @input="menu2 = false"></v-date-picker>
+          </v-menu>
+        </v-col>
+        <!-- Start the time_start of meeting -->
+        <v-col>
+          <v-menu
+            ref="menu"
+            v-model="menu3"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            :return-value.sync="time"
+            transition="scale-transition"
+            offset-y
+            max-width="290px"
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
                 v-model="minuteDetail.time_start"
-                full-width
-                @click:minute="$refs.menu.save(time)"
-              ></v-time-picker>
-            </v-menu>
-          </v-col>
-          <!-- Start the time_end of meeting -->
-          <v-col>
-            <v-menu
-              ref="menu"
-              v-model="menu4"
-              :close-on-content-click="false"
-              :nudge-right="40"
-              :return-value.sync="time"
-              transition="scale-transition"
-              offset-y
-              max-width="290px"
-              min-width="290px"
-            >
-              <template v-slot:activator="{ on }">
-                <v-text-field
-                  v-model="minuteDetail.time_end"
-                  label="Start Time"
-                  readonly
-                  v-on="on"
-                  :shaped="shaped"
-                  :outlined="outlined"
-                  :rounded="rounded"
-                ></v-text-field>
-              </template>
-              <v-time-picker
-                v-if="menu4"
+                label="Start Time"
+                readonly
+                v-on="on"
+                :shaped="shaped"
+                :outlined="outlined"
+                :rounded="rounded"
+              ></v-text-field>
+            </template>
+            <v-time-picker
+              v-if="menu3"
+              v-model="minuteDetail.time_start"
+              full-width
+              @click:minute="$refs.menu.save(time)"
+            ></v-time-picker>
+          </v-menu>
+        </v-col>
+        <!-- Start the time_end of meeting -->
+        <v-col>
+          <v-menu
+            ref="menu"
+            v-model="menu4"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            :return-value.sync="time"
+            transition="scale-transition"
+            offset-y
+            max-width="290px"
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
                 v-model="minuteDetail.time_end"
-                full-width
-                @click:minute="$refs.menu.save(time)"
-              ></v-time-picker>
-            </v-menu>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-textarea
-            counter
-            v-model="minuteDetail.description"
-            label="Description"
-            :shaped="shaped"
-            :outlined="outlined"
-            :rounded="rounded"
-          ></v-textarea>
-        </v-row>
-        <v-row>
-          <PeoplePicker v-bind:guests="minuteDetail.guests" />
-        </v-row>
-      </v-container>
-    </v-badge>
+                label="Start Time"
+                readonly
+                v-on="on"
+                :shaped="shaped"
+                :outlined="outlined"
+                :rounded="rounded"
+              ></v-text-field>
+            </template>
+            <v-time-picker
+              v-if="menu4"
+              v-model="minuteDetail.time_end"
+              full-width
+              @click:minute="$refs.menu.save(time)"
+            ></v-time-picker>
+          </v-menu>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-textarea
+          counter
+          v-model="minuteDetail.description"
+          label="Description"
+          :shaped="shaped"
+          :outlined="outlined"
+          :rounded="rounded"
+        ></v-textarea>
+      </v-row>
+      <v-row>
+        <PeoplePicker v-bind:guests="minuteDetail.guests" />
+      </v-row>
+    </v-container>
   </v-form>
 </template>
 
@@ -171,6 +183,41 @@ export default {
       this.componentVisible = true;
     }
   },
-  methods: {}
+  methods: {
+    add: function() {
+      console.log("Minute Detail", this.minuteDetail);
+      console.log("Guests", this.people);
+      // this.showLoader = true;
+      // axios
+      //   .post(
+      //     "https://localhost:8080/Development/?action=Login",
+      //     { data: postBody },
+      //     {
+      //       headers: {
+      //         "content-type": "application/json",
+      //         "Access-Control-Allow-Origin": "*"
+      //       }
+      //     }
+      //   )
+      //   .then(response => {
+      //     this.isLoggedOn = true;
+      //     var responseData = response.data.replace(/'/g, '"');
+      //     localStorage.jwt = JSON.parse(responseData).token;
+      //     this.clear();
+      //     this.showLoader = false;
+      //     this.$store.commit("setUser");
+      //     this.$router.push({ name: "Dashboard" });
+      //   })
+      //   .catch(() => {
+      //     this.failAlert = true;
+      //     this.clear();
+      //     this.showLoader = false;
+      //   });
+    },
+    clear: function() {
+      //Clear whatever is in form
+      this.minuteDetail = {};
+    }
+  }
 };
 </script>
