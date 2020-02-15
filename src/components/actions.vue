dc817ebfe50ad2e8875f9181ad463574cfd41676
Robert Cicero <robert.cicero.rc@gmail.com>
2020-02-15 18:31:08 +0000

    Working on adding actions

--- src/components/actions.vue
+++ src/components/actions.vue
@@ -3,9 +3,30 @@
     <!-- Row for header and button -->
     <v-row>
       <v-col>
-        <h3>Actions</h3>
+        <h1>Actions</h1>
       </v-col>
-      <v-spacer></v-spacer>
+      <v-col class="text-right">
+        <v-progress-circular
+          :indeterminate="indeterminate"
+          :rotate="rotate"
+          :size="size"
+          :value="value"
+          :width="width"
+          color="light-blue"
+          v-if="showLoader == true"
+        ></v-progress-circular>
+
+        <v-btn
+          class="ma-2"
+          tile
+          outlined
+          color="green"
+          @click="addAction"
+          :disabled="disableFields == true"
+        >
+          <v-icon left>mdi-pencil</v-icon>Add
+        </v-btn>
+      </v-col>
     </v-row>
     <!-- This row will be where you add a new actions -->
     <v-row>
@@ -21,9 +42,6 @@
       <v-col md="3">
         <PeoplePicker />
       </v-col>
-      <v-col class="text-right" md="1">
-        <v-btn @click="addAction"> Add </v-btn>
-      </v-col>
     </v-row>
     <!-- This row will for existing actions -->
     <v-row>
@@ -31,21 +49,22 @@
         <v-list two-line subheader>
           <v-list-item-group>
             <v-list-item v-for="item in actions" :key="item.id">
-              <template v-slot:default="{ active, toggle }">
-                <v-list-item-action>
-                  <v-checkbox
-                    v-model="active"
-                    color="primary"
-                    @click="toggle"
-                  ></v-checkbox>
-                </v-list-item-action>
-                <v-list-item>
-                  <v-list-item-content>
-                    <v-list-item-title v-text="item.description" />
-                    <v-list-item-subtitle v-text="item.assignee" />
-                  </v-list-item-content>
-                </v-list-item>
-              </template>
+              <v-list-item-action>
+                <v-checkbox
+                  v-model="item.checked"
+                  color="primary"
+                  @change="toggle(item)"
+                ></v-checkbox>
+              </v-list-item-action>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.description" />
+                  <v-list-item-subtitle v-text="item.assignee" />
+                </v-list-item-content>
+                <v-btn outlined color="error" :disabled="disableFields == true">
+                  <v-icon left>mdi-pencil</v-icon>Remove
+                </v-btn>
+              </v-list-item>
             </v-list-item>
           </v-list-item-group>
         </v-list>
@@ -62,7 +81,10 @@ export default {
   components: { PeoplePicker },
   data() {
     return {
+      disableFields: false,
+      showLoader: false,
       settings: [],
+      removeActive: false,
       actionDescription: "",
       actions: [
         {
@@ -95,6 +117,13 @@ export default {
     },
     clearActions: function() {
       this.actionDescription = "";
+    },
+    toggle: function(actionItem) {
+      let foundIndex = this.actions.findIndex(x => x.id === actionItem.id);
+      let newObject = this.actions[foundIndex];
+      if (newObject.checked === true) newObject.checked = false;
+      if (newObject.checked === false) newObject.checked = true;
+      this.actions[foundIndex] = newObject;
     }
   }
 };
