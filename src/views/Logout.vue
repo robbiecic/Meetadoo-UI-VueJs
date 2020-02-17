7a0c93786252f85579110c81c3e34c93998680e9
Robert Cicero <robert.cicero@iress.com>
2020-02-17 17:53:03 +0000

    Working on logout

--- src/views/Logout.vue
+++ src/views/Logout.vue
@@ -1,23 +1,36 @@
 <template>
   <v-app>
-    <v-content>
-      <Login />
-    </v-content>
+    <v-content>Content here</v-content>
   </v-app>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "Logout",
-
-  components: {},
-
-  created: {
-    //Need to update cookie so it's expired
+  data() {
+    return { successful: false };
   },
-
-  data: () => ({
-    //
-  })
+  created: function() {
+    //Need to update cookie so it's expired
+    // this.showLoader = true;
+    axios
+      .post(
+        "https://localhost:8080/Development/?action=Logout",
+        { data: {} },
+        {
+          headers: {
+            "content-type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+          }
+        }
+      )
+      .then(response => {
+        console.log(response);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
 };
 </script>
