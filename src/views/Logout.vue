6e140091bf13b94ad940f1556618cf311e83b5e0
Robert Cicero <robert.cicero@iress.com>
2020-02-18 07:56:01 +0000

    Logout now working nicely

--- src/views/Logout.vue
+++ src/views/Logout.vue
@@ -1,14 +1,23 @@
 <template>
   <v-app>
     <v-content>
-      <v-progress-circular
-        :indeterminate="indeterminate"
-        :rotate="rotate"
-        :size="size"
-        :value="value"
-        :width="width"
-        color="light-blue"
-      ></v-progress-circular>
+      <div class="text-center" v-if="showLoader == true">
+        <v-progress-circular
+          :indeterminate="indeterminate"
+          :rotate="rotate"
+          :size="size"
+          :value="value"
+          :width="width"
+          color="light-blue"
+        ></v-progress-circular>
+      </div>
+      <v-alert
+        v-if="unsuccessful == true"
+        type="error"
+      >Oops... Something went wrong, please try again later.</v-alert>
+      <v-container v-if="successful == true">
+        <v-alert type="success" dismissible>You have successfully logged out</v-alert>
+      </v-container>
     </v-content>
   </v-app>
 </template>
@@ -18,11 +27,20 @@ import axios from "axios";
 export default {
   name: "Logout",
   data() {
-    return { successful: false };
+    return {
+      successful: false,
+      showLoader: false,
+      unsuccessful: false,
+      indeterminate: true,
+      rotate: 0,
+      size: 32,
+      value: 0,
+      width: 4
+    };
   },
   created: function() {
     //Need to update cookie so it's expired
-    // this.showLoader = true;
+    this.showLoader = true;
     axios
       .post(
         "https://localhost:8080/Development/?action=Logout",
@@ -36,11 +54,16 @@ export default {
           }
         }
       )
-      .then(response => {
+      .then(() => {
+        this.$store.commit("unSetUser");
         this.successful = true;
+        this.unsuccessful = false;
+        this.showLoader = false;
       })
-      .catch(e => {
-        console.log(e);
+      .catch(() => {
+        this.successful = true;
+        this.unsuccessful = true;
+        this.showLoader = false;
       });
   }
 };
