783d66ed15da373ca1256c77b73ebe0b8a40add6
Robert Cicero <robert.cicero.rc@gmail.com>
2020-02-22 16:35:55 +0000

    proxy not working

--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,7 @@ module.exports = {
     https: true,
     proxy: {
       "/": {
-        target: "https://i6vtmh1eq3.execute-api.ap-southeast-2.amazonaws.com",
-        ws: true,
-        changeOrigin: true
+        target: "https://i6vtmh1eq3.execute-api.ap-southeast-2.amazonaws.com"
       }
     }
   }
