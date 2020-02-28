6597e2b76320a6c23fabbc9b4942c8141cd97b6b
Robert Cicero <robert.cicero@iress.com>
2020-02-28 16:38:28 +0000

    testing changes

--- tests/main.js
+++ tests/main.js
@@ -1,5 +1,6 @@
 module.exports = {
   "Loading website works": function(browser) {
+    let firstname = "";
     browser
       .url("https://www.meetadoo.com/")
       .waitForElementVisible("body")
@@ -11,6 +12,17 @@ module.exports = {
       .click("button[data-test=button_submit_login]")
       .assert.containsText("body", "My Profile", "Test passed: Login")
       //Try and update firstname
+      .clearValue("css selector", "input[data-test=text_firstname]")
+      .setValue("input[data-test=text_firstname]", "Robert")
+      .keys(browser.Keys.ENTER)
+      .assert.containsText("input[data-test=text_firstname]", "Robert")
+      // .getValue(
+      //   ("input[data-test=text_firstname]",
+      //   function(result) {
+      //     firstname = result.value;
+      //   })
+      // )
+      // .assert(firstname, "Robert")
       .end();
   }
 };
