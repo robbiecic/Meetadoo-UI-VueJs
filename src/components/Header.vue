6a589a82eeadc6c1d8a01a73b591259544f7865e
Robert Cicero <robert.cicero.rc@gmail.com>
2020-02-29 13:22:47 +0000

    Added new logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,80 +1,72 @@
 <template>
-  <v-card class="overflow-hidden">
-    <v-app-bar app color="#01adee">
-      <div class="d-flex align-center">
-        <v-img
-          alt="Meetadoo Logo"
-          class="shrink mr-2"
-          contain
-          transition="scale-transition"
-          width="40"
-        />
-      </div>
+  <v-app-bar app color="#01adee" class="pa-0">
+    <div class="d-flex align-left">
+      <v-img
+        alt="Meetadoo Logo"
+        contain
+        :src="require('../assets/logo_metadoo.png')"
+        transition="scale-transition"
+        max-height="232"
+        max-width="232"
+      ></v-img>
+    </div>
 
-      <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
 
-      <v-btn text to="/" data-test="button_home">
-        <span class="mr-2">Home</span>
-      </v-btn>
-      <v-btn
-        text
-        v-if="isLoggedOn == false"
-        to="/register"
-        data-test="button_register"
+    <v-btn text to="/" data-test="button_home">
+      <span class="mr-2">Home</span>
+    </v-btn>
+    <v-btn
+      text
+      v-if="isLoggedOn == false"
+      to="/register"
+      data-test="button_register"
+    >
+      <span class="mr-2">Register</span>
+    </v-btn>
+    <v-btn text v-if="isLoggedOn == false" to="/login" data-test="button_login">
+      <span class="mr-2">Login</span>
+    </v-btn>
+    <v-btn
+      text
+      v-if="isLoggedOn == true"
+      to="/dashboard"
+      data-test="button_dashboard"
+    >
+      <span class="mr-2">Dashboard</span>
+    </v-btn>
+    <v-btn
+      text
+      v-if="isLoggedOn == true"
+      to="/logout"
+      data-test="button_logout"
+    >
+      <span class="mr-2">Logout</span>
+    </v-btn>
+    <v-btn
+      href="https://github.com/vuetifyjs/vuetify/releases/latest"
+      target="_blank"
+      text
+    >
+      <span class="mr-2">Latest Release</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+    <template
+      v-slot:extension
+      v-if="$route.path == '/dashboard' || $route.path == '/dashboard/minutes'"
+    >
+      <v-tabs
+        grow
+        v-model="active_tab"
+        dark
+        background-color="blue-grey darken-4"
       >
-        <span class="mr-2">Register</span>
-      </v-btn>
-      <v-btn
-        text
-        v-if="isLoggedOn == false"
-        to="/login"
-        data-test="button_login"
-      >
-        <span class="mr-2">Login</span>
-      </v-btn>
-      <v-btn
-        text
-        v-if="isLoggedOn == true"
-        to="/dashboard"
-        data-test="button_dashboard"
-      >
-        <span class="mr-2">Dashboard</span>
-      </v-btn>
-      <v-btn
-        text
-        v-if="isLoggedOn == true"
-        to="/logout"
-        data-test="button_logout"
-      >
-        <span class="mr-2">Logout</span>
-      </v-btn>
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-      <template
-        v-slot:extension
-        v-if="
-          $route.path == '/dashboard' || $route.path == '/dashboard/minutes'
-        "
-      >
-        <v-tabs
-          grow
-          v-model="active_tab"
-          dark
-          background-color="blue-grey darken-4"
-        >
-          <v-tab v-for="tab of tabs" :key="tab.id" :to="tab.link">{{
-            tab.name
-          }}</v-tab>
-        </v-tabs>
-      </template>
-    </v-app-bar>
-  </v-card>
+        <v-tab v-for="tab of tabs" :key="tab.id" :to="tab.link">{{
+          tab.name
+        }}</v-tab>
+      </v-tabs>
+    </template>
+  </v-app-bar>
 </template>
 
 <script>
@@ -107,5 +99,8 @@ nav li.router-link-active,
 nav li.router-link-exact-active {
   background-color: indianred;
   cursor: pointer;
+}
+/deep/ .v-toolbar__content {
+  padding: 0px !important;
 }
 </style>
